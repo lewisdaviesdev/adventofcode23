6c60bc3741777a6235a4e027f143b8aff321feba
Lewis Davies <lewis.davies@geodesys.com>
2023-12-15 18:06:55 +0000

    day1-3

---
+++ advent-of-code/day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(
+    File.ReadAllLines("C:/Repos/adventofcode23/advent-of-code/day1/dataexport.txt")
+        .Select(input => Regex.Matches(input, "(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))")
+            .Select(m => (len: m.Groups[1].Length, dig: m.Groups[1].Value switch
+            {
+                "1" or "one" => 1,
+                "2" or "two" => 2,
+                "3" or "three" => 3,
+                "4" or "four" => 4,
+                "5" or "five" => 5,
+                "6" or "six" => 6,
+                "7" or "seven" => 7,
+                "8" or "eight" => 8,
+                "9" or "nine" => 9,
+                _ => throw new Exception(),
+            }))
+            .Aggregate<(int len, int dig), (int part1digit1, int part1digit2, int part2digit1, int part2digit2)>
+            ((-1, -1, -1, -1), (s, m) => m.len == 1 ?
+                (
+                    (s.part2digit1 < 0) ? (m.dig, m.dig, m.dig, m.dig) :
+                    (s.part1digit1 < 0) ? (m.dig, m.dig, p2d1: s.part2digit1, m.dig) :
+                    (s.part1digit1, m.dig, p2d1: s.part2digit1, m.dig)
+                ) :
+                (
+                    (s.part2digit1 < 0) ? (p1d1: s.part1digit1, p1d2: s.part1digit2, m.dig, m.dig) :
+                        (p1d1: s.part1digit1, p1d2: s.part1digit2, s.part2digit1, m.dig)
+                )
+            )
+        )
+        .Select(agg => (part1: agg.part1digit1 * 10 + agg.part1digit2, part2: agg.part2digit1 * 10 + agg.part2digit2))
+        .Aggregate( (part1: 0, part2: 0), (acc, x) => (acc.part1 + x.part1, acc.part2 + x.part2))
+        .ToString()
+);
---
+++ advent-of-code/day2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+
+(int, int, int) initialCubes = (12, 13, 14);
+
+var part1Sum = File.ReadAllText("C:/Repos/adventofcode23/advent-of-code/day2/data/day2.txt")
+    .Split("\n")
+    .Select(line => new Game(line))
+    .Where(game => game.IsValid(initialCubes))
+    .Aggregate(0, (sum, game) => sum + game.IdNumber);
+
+Console.WriteLine(part1Sum);
+
+var part2Sum = File.ReadAllText("C:/Repos/adventofcode23/advent-of-code/day2/data/day2.txt")
+    .Split("\n")
+    .Select(line => new Game(line))
+    .Select(game => game.GetSmallestPossibleConfiguration())
+    .Select(tuple => tuple.Item1 * tuple.Item2 * tuple.Item3)
+    .Aggregate(0, (sum, power) => sum + power);
+
+Console.WriteLine(part2Sum);
+
+internal readonly struct Game
+{
+    public readonly int IdNumber { get; }
+    
+    //  (red, green, blue) cubes.
+    public readonly IEnumerable<(int, int, int)> RgbTuples { get; }
+
+    //  Assumes perfectly formatted input 
+    public Game(string information)
+    {
+        IdNumber = int.Parse(new Regex(@"\d+").Match(new Regex(@"Game +\d+: +").Match(information).Value).Value);
+
+        information = Regex.Replace(information, @"Game +\d+: +", "");  //  Removes the 'Game x:' prefix
+        information = Regex.Replace(information, " +", "");             //  Removes all whitespaces
+
+        RgbTuples = information
+            .Split(";")
+            .Select(GetTuple);
+    }
+
+    public bool IsValid((int, int, int) initialConfiguration)
+    {
+        return RgbTuples.All(tuple => tuple.Item1 <= initialConfiguration.Item1 && 
+                               tuple.Item2 <= initialConfiguration.Item2 &&
+                               tuple.Item3 <= initialConfiguration.Item3);
+    }
+
+    public (int, int, int) GetSmallestPossibleConfiguration()
+    {
+        (int, int, int) currentTuple = (0, 0, 0);
+
+        foreach (var tuple in RgbTuples)
+        {
+            currentTuple = 
+                (Int32.Max(currentTuple.Item1, tuple.Item1), 
+                Int32.Max(currentTuple.Item2, tuple.Item2),
+                Int32.Max(currentTuple.Item3, tuple.Item3));
+        }
+
+        return currentTuple;
+    }
+
+    //  Also assumes perfectly formatted input
+    private static (int, int, int) GetTuple(string rawTupleInformation)
+    {
+        Match redCubesMatch = new Regex(@"\d+red").Match(rawTupleInformation);
+        int redCubes = redCubesMatch.Success
+            ? int.Parse(new Regex(@"\d+").Match(redCubesMatch.Value).Value)
+            : 0;
+        
+        Match greenCubesMatch = new Regex(@"\d+green").Match(rawTupleInformation);
+        int greenCubes = greenCubesMatch.Success
+            ? int.Parse(new Regex(@"\d+").Match(greenCubesMatch.Value).Value)
+            : 0;
+        
+        Match blueCubesMatch = new Regex(@"\d+blue").Match(rawTupleInformation);
+        int blueCubes = blueCubesMatch.Success
+            ? int.Parse(new Regex(@"\d+").Match(blueCubesMatch.Value).Value)
+            : 0;
+        
+        return (redCubes, greenCubes, blueCubes);
+    }
+}
